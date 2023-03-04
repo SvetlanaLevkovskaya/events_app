0121cb5b94830c1dc063e4f6cea39e985c4e5385
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-03-04 22:12:55 +0300

    add event

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,6 +1,44 @@
-const Page = () => {
+import Image from "next/image";
+
+const Page = ({event}) => {
     return (
-      <h1> Our single Event</h1>
-    )
-}
+      <div>
+          <Image src={event.image} width={1000} height={500} alt={event.title} />
+          <h1>{event.title}</h1>
+          <p>{event.description}</p>
+      </div>
+    );
+};
 export default Page;
+
+export async function getStaticPaths() {
+
+    const {allEvents} = await import("/data/data.json");
+
+    const allPaths = allEvents.map(path => {
+        return {
+            params: {
+                cat: path.city,
+                id: path.id,
+            }
+        };
+    });
+
+    return {
+        paths: allPaths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps(context) {
+
+    const {allEvents} = await import("/data/data.json");
+
+    const event = allEvents.find(event => event.id === context.params.id);
+
+    return {
+        props: {
+            event,
+        }
+    };
+}
